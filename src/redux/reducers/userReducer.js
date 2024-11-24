caebf68b7db108e99417e188c7826d41ab54989f
AeroHopper <rodrigosantiagopimentel@gmail.com>
2024-11-24 11:18:32 +0900

    update

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -12,19 +12,6 @@ import { ADD_USER, DELETE_USER, EDIT_USER } from '@/constants/constants';
 //   }
 // ];
 
-// import profile from 'static/profile.jpg';
-// import banner from 'static/banner.jpg';
-
-// const initState = {
-//   fullname: 'Pedro Juan',
-//   email: 'juanpedro@gmail.com',
-//   address: '',
-//   mobile: {},
-//   avatar: profile,
-//   banner,
-//   dateJoined: 1954234787348
-// };
-
 export default (state = {}, action) => {
   switch (action.type) {
     case ADD_USER:
